bd0cbda8bc60f14142d64b32cd678a73e75e2dc4
suraj <surajbanthot@gmail.com>
2020-10-18 11:49:54 +0530

    Week_3/string:

--- Week_3/string.cpp
+++ Week_3/string.cpp
@@ -1,14 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string s1="Prep";
-    string s2 ="Bytes";
+    string s1 = "PrepBuddy";
+    string s2 = "Byteses";
     string s3 = "RFunction";
     string s4;
 
-    s4=s1;
+    // s4 = s1;
     // string::iterator sit;
     // cout<<"String using begin() and end()"<<endl;
 
@@ -16,12 +16,35 @@ int main()
     // cout<<*sit;
     // cout<<endl;
 
-    string:: reverse_iterator rsit;
-    cout<<"string using rbegin() and rend()"<<endl;
+    // string:: reverse_iterator rsit;
+    // cout<<"string using rbegin() and rend()"<<endl;
 
-    for(rsit=s1.rbegin();rsit!=s1.rend();rsit++)
-    cout<<*rsit;
-    cout<<endl;
+    // for(rsit=s1.rbegin();rsit!=s1.rend();rsit++)
+    // cout<<*rsit;
+    // cout<<endl;
+
+    // char ch[8];
+    // s1.copy(ch,5,2);
+    // cout<<ch<<endl;
+    // cout << s1 << endl;
+    // s1.swap(s3);
+    // cout << s1 << endl;
+
+
+    cout<<"First Occurence of es in s2"<<endl;
+    cout<<s2.find("es")<<endl;
+    cout<<"last occurence of is in s2 "<<s2.rfind("es")<<endl;
+
+    cout<<s2.find_first_of("es")<<endl;
+
+    s2.insert(3, "Prep");
+    cout<<s2<<endl;
+
+    s2.clear();
+    if(s2.empty())
+    cout<<"String is empty\n";
+
+    
 
     return 0;
 }
