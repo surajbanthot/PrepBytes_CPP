d0e096cc7f87642f050a12d0243b9259fd18a460
suraj <surajbanthot@gmail.com>
2020-10-31 11:37:52 +0530

    Week_3/STRINGROT:

--- Week_3/STRINGROT.cpp
+++ Week_3/STRINGROT.cpp
@@ -18,43 +18,42 @@ int main()
         cin >> l >> r >> k;
 
         k = k % (r - l + 1);
-        cout <<" k "<< k << endl;
+        cout << " k " << k << endl;
         if (k != 0)
         {
             st = r - k + 1;
-            cout<<" st "<<st<<endl;
+            cout << " st " << st << endl;
             st--;
-           cout<<"st "<<st<<endl;
+            cout << "st " << st << endl;
             for (j = st; j < r; j++)
             {
-                cout<<"j "<<j<<" s[j] "<<s[j];
+                cout << "j " << j << " s[j] " << s[j];
                 t.push_back(s[j]);
-                
-                cout<<" t "<<t<<endl;
+
+                cout << " t " << t << endl;
             }
 
             for (j = l - 1; j < st; j++)
             {
-                cout<<" j "<<j<<" s[j] "<<s[j];
+                cout << " j " << j << " s[j] " << s[j];
                 t.push_back(s[j]);
-                cout<<" t "<<t<<endl;
+                cout << " t " << t << endl;
             }
 
             int idx = 0;
             for (j = l - 1; j < r; j++)
             {
-                
+
                 s[j] = t[idx];
-                cout<<" s[j] "<<s[j];
+                cout << " s[j] " << s[j];
                 idx++;
-                cout<<" idx "<<idx<<endl;
-                
+                cout << " idx " << idx << endl;
             }
-            cout<<" s "<<s<<endl;
+            cout << " s " << s << endl;
         }
     }
 
-    cout <<"s "<< s;
+    cout << "s " << s;
 
     return 0;
 }
